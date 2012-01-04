6b6aaf4e801b1f8480387dbbc43495f7dc5dff78
Chris Baker <baker@svnetwork.com>
2012-01-04 13:51:05 -0800

    ActiveSupport::Concern 3.2 no longer uses the InstanceMethods module

--- lib/representation.rb
+++ lib/representation.rb
@@ -29,44 +29,42 @@ module Representation
     end
   end
 
-  module InstanceMethods
-    def representation(name)
-      attributes_and_method_names = self.class.values_for_representation(name)
-      represented_attributes = attributes_and_method_names.inject({}) do |attributes, attribute|
-        value = send(attribute)
+  def representation(name)
+    attributes_and_method_names = self.class.values_for_representation(name)
+    represented_attributes = attributes_and_method_names.inject({}) do |attributes, attribute|
+      value = send(attribute)
 
-        if value.respond_to? :each
-          value = value.map do |v|
-            v.respond_to?(:representation) ? v.representation(name) : v
-          end
-        else
-          value = value.respond_to?(:representation) ? value.representation(name) : value
+      if value.respond_to? :each
+        value = value.map do |v|
+          v.respond_to?(:representation) ? v.representation(name) : v
         end
-
-        attributes.merge({attribute.to_s => value})
+      else
+        value = value.respond_to?(:representation) ? value.representation(name) : value
       end
 
-      clone.tap do |represented_object|
-        represented_object.instance_variable_set('@attributes', represented_attributes)
-
-        represented_attributes.each do |key, value|
-          represented_object.singleton_class.send :attr_accessor, key.to_sym
-          represented_object.send "#{key}=".to_sym, value
-        end
-
-        def represented_object.attributes
-          @attributes
-        end
-
-        def represented_object.inspect
-          attributes_as_nice_string = @attributes.keys.map {|name| "#{name}: #{attribute_for_inspect(name)}"}
-          "#<#{self.class} #{attributes_as_nice_string.compact.join(", ")}>"
-        end
-      end
+      attributes.merge({attribute.to_s => value})
     end
 
-    alias_method :as, :representation
+    clone.tap do |represented_object|
+      represented_object.instance_variable_set('@attributes', represented_attributes)
+
+      represented_attributes.each do |key, value|
+        represented_object.singleton_class.send :attr_accessor, key.to_sym
+        represented_object.send "#{key}=".to_sym, value
+      end
+
+      def represented_object.attributes
+        @attributes
+      end
+
+      def represented_object.inspect
+        attributes_as_nice_string = @attributes.keys.map {|name| "#{name}: #{attribute_for_inspect(name)}"}
+        "#<#{self.class} #{attributes_as_nice_string.compact.join(", ")}>"
+      end
+    end
   end
+
+  alias_method :as, :representation
 end
 
 ActiveSupport.on_load(:active_record) { include Representation }
